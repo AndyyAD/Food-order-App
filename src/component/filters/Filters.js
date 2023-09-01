c6d51c7bfcb6c02276aece3db51bc80ae4d6f90b
Andy <ashishdeb1231@gmail.com>
2023-09-01 23:52:01 +0530

    Wrote the filter function

--- src/component/filters/Filters.js
+++ src/component/filters/Filters.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Filters.css'
 import '../../App.css'
 import food from '../../assets/dataset.json'
 
 const Filters = () => {
+    const [items, setItems] = useState('')
+    console.log(items)
+
     return (
         <section className='container'>
             <div className='filterWrapper'>
-                <div className='filterWrap selected'>
-                    <p className="filter"> All </p>
+                <div className='filterWrap selected' onClick={() => setItems('')}>
+                    <p className="filters"> All </p>
                 </div>
                 {
-                    food.labels.map((data) => (
-                        <div className='filterWrap' key={data.id}>
-                            <p className="filter"> {data.label} </p>
+                    food.labels.map((data) => (                        
+                        <div className='filterWrap' key={data.id} onClick={() => setItems(data.label)}>
+                            <p className="filters"> {data.label} </p>
                         </div>
                     ))
                 }
