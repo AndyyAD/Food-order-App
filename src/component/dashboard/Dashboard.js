fb1271ae66a1d50a5d8c47be91757e6a5a537641
Andy <ashishdeb1231@gmail.com>
2023-09-04 21:27:29 +0530

    Applied sected filter animation

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -8,19 +8,23 @@ import Beer from './Beer.png'
 
 function Dashboard() {
     const [items, setItems] = useState('');
-    const [drink, setDrink] = useState('');
-    const [drinkPrice, setDrinkPrice] = useState(0)
+    const [toggle, setToggle] = useState('');
+
+    const toggleBTN = (index) => {
+        setItems(index)
+        setToggle(index)
+    }
 
     return (
         <>
             <section className='container'>
                 <div className='filterWrapper'>
-                    <div className='filterWrap selected' onClick={() => setItems('')}>
+                    <div className={`filterWrap ${toggle === '' ? "selected" : null}`} onClick={() => toggleBTN('')}>
                         <p className="filters"> All </p>
                     </div>
                     {
                         food.labels.map((data) => (
-                            <div className='filterWrap' key={data.id} onClick={() => setItems(data.label)}>
+                            <div className={`filterWrap ${toggle === data.label ? "selected" : null}`} key={data.id} onClick={() => toggleBTN(data.label)}>
                                 <p className="filterName"> {data.label} </p>
                             </div>
                         ))
@@ -33,7 +37,7 @@ function Dashboard() {
                     {
                         food.meals.map((data) => {
                             if (data.title.includes(items)) {
-                                return (
+                                return ( 
                                     <div className="foodWrapper" key={data.id}>
                                         <div>
                                             <img className='foodImg' src={data.img} alt="Image of mentioned food" />
@@ -43,15 +47,15 @@ function Dashboard() {
                                             <h1 className="foodDesc"> {data.description} </h1>
                                             <p className="starter"> <strong>Starter:</strong> {data.starter} </p>
                                             <p className="desert"> <strong>Desert:</strong> {data.desert} </p>
-                                            <p className="drinks"> <strong>Selected drink:</strong> {drink} </p>
+                                            <p className="drinks"> <strong>Selected drink:</strong> </p>
                                             <div className="moreInfo">
                                                 <div className="drinkImgWrapper">
-                                                    <img className="drinkImg" src={Wine} alt="Glass of wine" />
+                                                    <img className="drinkImg" src={Wine} alt="Glass of wine"/>
                                                     <img className="drinkImg" src={Juice} alt="Glass of juice"/>
                                                     <img className="drinkImg" src={Beer} alt="Mug of beer"/>
                                                 </div>
                                                 <div className="order">
-                                                    <h1 className='price'> ${data.price + drinkPrice} </h1>
+                                                    <h1 className='price'> ${data.price} </h1>
                                                     <button className='submitBTN'> Select </button>
                                                 </div>
                                             </div>
