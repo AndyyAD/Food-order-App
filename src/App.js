33b0eddf96dc2c755052fbe6b11e01945ce04356
Andy <ashishdeb1231@gmail.com>
2023-09-04 15:19:18 +0530

    Made the filters work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import Dashboard from './component/dashboard/Dashboard';
-import Filters from './component/filters/Filters';
 
 function App() {
     return (
         <>
-            <Filters />
             <Dashboard />
         </>
     );
