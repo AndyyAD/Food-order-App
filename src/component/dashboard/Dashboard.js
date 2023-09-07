e71ed738e68b3aca04e02712d185002a06794ce0
Andy <ashishdeb1231@gmail.com>
2023-09-07 17:23:53 +0530

    Made drink select feature

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react'
 import './Dashboard.css'
 import '../../App.css'
+import food from '../../assets/dataset.json'
 import Wine from './Wine.png'
 import Juice from './Juice.png'
 import Beer from './Beer.png'
 
 function Dashboard({ items }) {
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [mainId, setMainId] = useState('')
+    const [drinkId, setDrinkId] = useState('')
+    const [toggle1, setToggle1] = useState(false)
+
     const drinkImgData = [
         {
             "img": Wine,
-            "alt": "Glass of wine"
+            "alt": "Glass of vine"
         },
         {
             "img": Juice,
@@ -20,6 +27,24 @@ function Dashboard({ items }) {
             "alt": "Glass of beer"
         }
     ]
+
+    const drinksData = food.meals[0].drinks;
+    const drinksWithImg = drinkImgData.map((drinkImgData, index) => ({ ...drinkImgData, ...drinksData[index] }))
+
+    const selectDrink = (mainId, drinkId, title, price) => {
+        if (toggle1) {
+            setName('')
+            setPrice(0)
+            setMainId('')
+            setDrinkId('')
+        } else {
+            setName(title)
+            setPrice(price)
+            setMainId(mainId)
+            setDrinkId(drinkId)
+        }
+        setToggle1(toggle1 => !toggle1)
+    }
 
     return (
         <div className="dashboardWrapper">
@@ -35,20 +60,23 @@ function Dashboard({ items }) {
                                 <h1 className="foodDesc"> {data.description} </h1>
                                 <p className="starter"> <strong>Starter:</strong> {data.starter} </p>
                                 <p className="desert"> <strong>Desert:</strong> {data.desert} </p>
-                                <p className="drinks"> <strong>Selected drink:</strong>  </p>
+                                <p className="drinks"> <strong>Selected drink:</strong> {data.id === mainId ? name : ''}</p>
                                 <div className="moreInfo">
-                                    <div className="drinkImgWrapper">
+                                    <div className="drinkImgWrapper ">
                                         {
-                                            drinkImgData.map((drink) => (
-                                                <img
-                                                    className={`drinkImg`}
-                                                    src={drink.img} alt={drink.alt}
-                                                />
+                                            drinksWithImg.map((drink) => (
+                                                <div key={drink.id}>
+                                                    <img
+                                                        className={`drinkImg ${(mainId === data.id && drinkId === drink.id) ? 'selectedDrink' : ''}`}
+                                                        src={drink.img} alt={drink.alt}
+                                                        onClick={() => selectDrink(data.id, drink.id, drink.title, drink.price)}
+                                                    />
+                                                </div>
                                             ))
                                         }
                                     </div>
                                     <div className="order">
-                                        <h1 className='price'> ${data.price} </h1>
+                                        <h1 className='price'> ${(data.price + (data.id === mainId ? price : 0)).toFixed(2)} </h1>
                                         <button className='submitBTN'> Select </button>
                                     </div>
                                 </div>
